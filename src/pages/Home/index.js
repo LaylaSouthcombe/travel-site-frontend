e1ca107c6711d247c2469ab7f1789b2d347a1708
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-01-16 21:20:49 +0000

    add change of icon for nav bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,26 +3,32 @@ import { FirstSideNavBar, SecondSideNavBar } from '../../layout'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+
 
 const About = () => {
     const [selectedListTitle, setSelectedListTitle] = useState();
     const [secondNavBarDisplay, setSecondNavBarDisplay] = useState('hidden')
     const [listData, setListData] = useState([])
     const [sideMenuOpen, setSideMenuOpen] = useState(false)
-
+    const [menuIcon, setMenuIcon] = useState(faBars)
 
     const openCloseSideMenu = () => {
         if(sideMenuOpen){
             setSideMenuOpen(false)
+            setMenuIcon(faBars)
         } else if(!sideMenuOpen){
             setSideMenuOpen(true)
+            setMenuIcon(faXmark)
         }
     }
 
     return(
         <>
         <div className="sideMenuButton" onClick={(e) => openCloseSideMenu()}>
-            <FontAwesomeIcon icon={faEarthAmericas} />
+            <FontAwesomeIcon icon={menuIcon} />
         </div>
         {sideMenuOpen ? <div>
                 <FirstSideNavBar listData={listData} setListData={setListData} selectedListTitle={selectedListTitle} setSelectedListTitle={setSelectedListTitle} secondNavBarDisplay={secondNavBarDisplay} setSecondNavBarDisplay={setSecondNavBarDisplay}/>
