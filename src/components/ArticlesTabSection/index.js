df59ca77c2d514fb461a2b9d314b8d56dd6c6501
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-03-27 22:47:13 +0100

    add filter function for article tabs

--- src/components/ArticlesTabSection/index.js
+++ src/components/ArticlesTabSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Tab from '@material-ui/core/Tab';
@@ -11,39 +11,74 @@ import {ArticleTabCards} from '../index'
 const ArticlesTabSection = ({tabArticles, tabHeadings}) => {
     let navigate = useNavigate();
 
-    // console.log(tabArticles)
-
     const [value, setValue] = React.useState('1');
 
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
-  
+
+    const addKeysToSortedArticlesArray = (tabHeadings) => {
+        let sortedArticlesArray = {}
+        tabHeadings.forEach(heading => {
+            sortedArticlesArray[heading] = []
+        })
+        return sortedArticlesArray
+    }
+
+    const sortArticles = (articlesArray, heading) => {
+        for(let i = 0; i < tabArticles.length; i++){
+            if(heading === 'All'){
+                articlesArray['All'].push(tabArticles[i])
+            } else {
+                if(tabArticles[i]["trip_categories"].includes(heading)){
+                    articlesArray[heading].push(tabArticles[i])
+                }
+            }
+        }
+    }
+
+    const removeTabsWithNoArticles = (object) => {
+        Object.keys(object).forEach(key => {
+            if(!object[key].length > 0){
+                delete object[key]
+            }
+        })
+        return object
+    }
+
+    let sortedArticlesArray
+    sortedArticlesArray = addKeysToSortedArticlesArray(tabHeadings)
+
+    tabHeadings.forEach(heading => {
+        sortArticles(sortedArticlesArray, heading)
+    })
+    sortedArticlesArray = removeTabsWithNoArticles(sortedArticlesArray)  
+
     return (
         <div className="articleTabSection">
         <Box sx={{ width: '100%', typography: 'body1', height: "fit-content", marginBottom: "30px"}}>
             <TabContext value={value} sx={{padding: "12px"}}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <TabList variant="scrollable" scrollButtons="auto" onChange={handleChange} aria-label="lab API tabs example">
-                {tabHeadings.map((heading, i) => {
-                    return (
+                {Object.keys(sortedArticlesArray).map((heading, i) => {
+                        return (
                             <Tab label={heading} value={(i+1).toString()}/>
                         )
                     }
                 )}
                 </TabList>
             </Box>
-
-            {tabHeadings.map((heading, i) => {
+            {Object.keys(sortedArticlesArray).map((heading, i) => {
                     return (
-                        <TabPanel value={(i+1).toString()}>{tabArticles[i].map(article => {
+                        <TabPanel value={(i+1).toString()}>{sortedArticlesArray[heading].map(article => {
                             return (
                                 <ArticleTabCards article={article} classnames={"articleTabCard"}/>
                             )
-                        })}</TabPanel>
+                        })}
+                        </TabPanel>
                     )
-                    }
-                )}
+                }
+            )}
             </TabContext>
         </Box>
       </div>
