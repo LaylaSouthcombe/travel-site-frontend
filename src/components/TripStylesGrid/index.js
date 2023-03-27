913f3a376f6cc8a1699eec990b7b448e0a2698e6
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-03-27 21:00:02 +0100

    add hover effect to trip styles grid text

--- src/components/TripStylesGrid/index.js
+++ src/components/TripStylesGrid/index.js
@@ -18,54 +18,54 @@ const TripStylesGrid = () => {
     return (
         <div className="tripStyles">
             <div onClick={() => navigate('/trip-styles/city-breaks')}>
-            <div>
-               <img src={city} alt="Cityscape illustration" /> 
+                <div>
+                <img src={city} alt="Cityscape illustration" /> 
+                </div>
+                <p>City Break</p>
             </div>
-            <p>City Break</p>
-        </div>
-        <div onClick={() => navigate('/trip-styles/luxury')}>
-            <div>
-                <img src={luxury} alt="Camera with photographs illustration" />
+            <div onClick={() => navigate('/trip-styles/luxury')}>
+                <div>
+                    <img src={luxury} alt="Camera with photographs illustration" />
+                </div>
+                <p>Luxury</p>
             </div>
-            <p>Luxury</p>
-        </div>
-        <div onClick={() => navigate('/trip-styles/nature')}>
-            <div>
-               <img src={nature} alt="Leafs and nature illustration" /> 
+            <div onClick={() => navigate('/trip-styles/nature')}>
+                <div>
+                <img src={nature} alt="Leafs and nature illustration" /> 
+                </div>
+                <p>Nature</p>
             </div>
-            <p>Nature</p>
-        </div>
-        <div onClick={() => navigate('/trip-styles/food')}>
-            <div>
-               <img src={food} alt="Couple eating dinner outside illustration" /> 
+            <div onClick={() => navigate('/trip-styles/food')}>
+                <div>
+                <img src={food} alt="Couple eating dinner outside illustration" /> 
+                </div>
+                <p>Food</p>
             </div>
-            <p>Food</p>
-        </div>
-        <div onClick={() => navigate('/trip-styles/relaxing')}>
-            <div>
-                <img src={relaxing} alt="Person on beach illustration" />
+            <div onClick={() => navigate('/trip-styles/relaxing')}>
+                <div>
+                    <img src={relaxing} alt="Person on beach illustration" />
+                </div>
+                <p>Relaxing</p>
             </div>
-             <p>Relaxing</p>
-        </div>
-        <div onClick={() => navigate('/trip-styles/budget-friendly')}>
-            <div>
-               <img src={budget} alt="Piggy bank illustration" /> 
+            <div onClick={() => navigate('/trip-styles/budget-friendly')}>
+                <div>
+                <img src={budget} alt="Piggy bank illustration" /> 
+                </div>
+                <p>Budget Friendly</p>
             </div>
-            <p>Budget Friendly</p>
-        </div>
-        <div onClick={() => navigate('/trip-styles/art-and-culture')}>
-            <div>
-                <img src={art} alt="Person standing infront of art frame illustration" />
+            <div onClick={() => navigate('/trip-styles/art-and-culture')}>
+                <div>
+                    <img src={art} alt="Person standing infront of art frame illustration" />
+                </div>
+                <p>Art & Culture</p>
             </div>
-            <p>Art & Culture</p>
-        </div>
-        <div onClick={() => navigate('/trip-styles/adventure')}>
-            <div>
-              <img src={adventure} alt="Woman standing by mountains illustration" />  
+            <div onClick={() => navigate('/trip-styles/adventure')}>
+                <div>
+                <img src={adventure} alt="Woman standing by mountains illustration" />  
+                </div>
+                <p>Adventure</p>
             </div>
-            <p>Adventure</p>
         </div>
-    </div>
     )
 }
 
