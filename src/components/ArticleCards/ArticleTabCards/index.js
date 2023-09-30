962e72c4f5266c7c39819dc72b15e9370a514135
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-09-30 20:29:48 +0100

    split article tab card to smaller components

--- src/components/ArticleCards/ArticleTabCards/index.js
+++ src/components/ArticleCards/ArticleTabCards/index.js
@@ -2,43 +2,61 @@ import React, {Fragment} from 'react'
 import Skeleton from '@mui/material/Skeleton';
 import './style.css'
 
+const SkeletonArticleCard = ({classnames, keyId}) => {
+    return (
+        <div className={classnames + " skeletonArticleCard"} key={keyId}>
+            <div className="cardImg" data-testid="skeleton-image">
+                <Skeleton variant="rounded" width={"100%"} height={160}/>
+            </div>
+            <div className="threeCardInfo">
+                <p className="skeletonArticleTag" data-testid="skeleton-category">
+                    <Skeleton variant="rounded" width={"100%"} height={20}/>
+                </p>
+                <p className="articleCardTitle skeletonArticleCardTitle" maxLength="4" data-testid="skeleton-title">
+                    <Skeleton variant="rounded" width={"100%"} height={20}/>
+                    <Skeleton variant="rounded" width={"100%"} height={20}/>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+const ArticleTabCardImg = ({image}) => {
+    return (
+        <div className="cardImg">
+            <img src={image} alt=""/>
+        </div>
+    )
+}
+
+const ArticleTabCardMeta = ({category, title}) => {
+    return (
+        <div className="threeCardInfo">
+            <p className="articleCardCategoryArea articleCardCategory">{category}</p>
+            <p className="articleCardTitle articleTabCardTitle">
+                {title}
+            </p>
+        </div>
+    )
+}
+
 const ArticleTabCards = ({article, classnames, keyId, loaded}) => {
 
     return (
         <Fragment key={keyId}>
             {loaded ?
-            <Fragment key={keyId}>
+            <>
                 {article !== undefined ? 
-                <Fragment key={keyId}>
+                <>
                     <a className={classnames + " articleCard"} key={keyId} href={"/article/" + article.id} target="_blank" rel="noreferrer">
-                        <div className="cardImg">
-                            <img src={article.feature_img_url} alt=""/>
-                        </div>
-                        <div className="threeCardInfo">
-                            <p className="articleCardCategoryArea articleCardCategory">{article.article_category}</p>
-                            <p className="articleCardTitle articleTabCardTitle">
-                                {article.title}
-                            </p>
-                        </div>
+                        <ArticleTabCardImg image={article.feature_img_url}/>
+                        <ArticleTabCardMeta category={article.article_category} title={article.title}/>
                     </a>
-                </Fragment>
+                </>
                 : null}
-            </Fragment>
+            </>
             :
-                <div className={classnames + " skeletonArticleCard"} key={keyId}>
-                    <div className="cardImg" data-testid="skeleton-image">
-                        <Skeleton variant="rounded" width={"100%"} height={160}/>
-                    </div>
-                    <div className="threeCardInfo">
-                        <p className="skeletonArticleTag" data-testid="skeleton-category">
-                            <Skeleton variant="rounded" width={"100%"} height={20}/>
-                        </p>
-                        <p className="articleCardTitle skeletonArticleCardTitle" maxLength="4" data-testid="skeleton-title">
-                            <Skeleton variant="rounded" width={"100%"} height={20}/>
-                            <Skeleton variant="rounded" width={"100%"} height={20}/>
-                        </p>
-                    </div>
-                </div>
+            <SkeletonArticleCard classnames keyId/>
             }
         </Fragment>
     )
