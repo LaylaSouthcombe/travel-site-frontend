6d9fd11bb141235bffc0fff1f227be837952af55
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-09-30 18:50:06 +0100

    change trip styles grid to a tags

--- src/components/TripStylesGrid/index.js
+++ src/components/TripStylesGrid/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 
 import adventure from '../../images/TripStylesIcons/adventure.png'
 import art from '../../images/TripStylesIcons/art.png'
@@ -13,58 +12,68 @@ import relaxing from '../../images/TripStylesIcons/relaxing.png'
 import './style.css'
 
 const TripStylesGrid = () => {
-    let navigate = useNavigate();
+
+    const tripStyles = [
+        {
+            url: "/articles/category=city-break",
+            image: city,
+            alt: "Cityscape illustration",
+            text: "City Break"
+        },
+        {
+            url: "/articles/category=luxury",
+            image: luxury,
+            alt: "Camera with photographs illustration",
+            text: "Luxury"
+        },
+        {
+            url: "/articles/category=nature",
+            image: nature,
+            alt: "Leafs and nature illustration",
+            text: "Nature"
+        },
+        {
+            url: "/articles/category=food",
+            image: food,
+            alt: "Couple eating dinner outside illustration",
+            text: "Food"
+        },
+        {
+            url: "/articles/category=relaxing",
+            image: relaxing,
+            alt: "Person on beach illustration",
+            text: "Relaxing"
+        },
+        {
+            url: "/articles/category=budget-friendly",
+            image: budget,
+            alt: "Piggy bank illustration",
+            text: "Budget Friendly"
+        },
+        {
+            url: "/articles/category=art-and-culture",
+            image: art,
+            alt: "Person standing infront of art frame illustration",
+            text: "Art & Culture"
+        },
+        {
+            url: "/articles/category=adventure",
+            image: adventure,
+            alt: "Woman standing by mountains illustration",
+            text: "Adventure"
+        }
+    ]
 
     return (
         <div className="tripStyles">
-            <div onClick={() => navigate('/trip-styles/city-breaks')}>
-                <div>
-                <img src={city} alt="Cityscape illustration" /> 
-                </div>
-                <p>City Break</p>
-            </div>
-            <div onClick={() => navigate('/trip-styles/luxury')}>
-                <div>
-                    <img src={luxury} alt="Camera with photographs illustration" />
-                </div>
-                <p>Luxury</p>
-            </div>
-            <div onClick={() => navigate('/trip-styles/nature')}>
-                <div>
-                <img src={nature} alt="Leafs and nature illustration" /> 
-                </div>
-                <p>Nature</p>
-            </div>
-            <div onClick={() => navigate('/trip-styles/food')}>
-                <div>
-                <img src={food} alt="Couple eating dinner outside illustration" /> 
-                </div>
-                <p>Food</p>
-            </div>
-            <div onClick={() => navigate('/trip-styles/relaxing')}>
-                <div>
-                    <img src={relaxing} alt="Person on beach illustration" />
-                </div>
-                <p>Relaxing</p>
-            </div>
-            <div onClick={() => navigate('/trip-styles/budget-friendly')}>
-                <div>
-                <img src={budget} alt="Piggy bank illustration" /> 
-                </div>
-                <p>Budget Friendly</p>
-            </div>
-            <div onClick={() => navigate('/trip-styles/art-and-culture')}>
-                <div>
-                    <img src={art} alt="Person standing infront of art frame illustration" />
-                </div>
-                <p>Art & Culture</p>
-            </div>
-            <div onClick={() => navigate('/trip-styles/adventure')}>
-                <div>
-                <img src={adventure} alt="Woman standing by mountains illustration" />  
-                </div>
-                <p>Adventure</p>
-            </div>
+            {tripStyles.map(style => (
+                <a href={style.url} target="_blank" rel="noreferrer" key={style.text}>
+                    <div>
+                    <img src={style.image} alt={style.alt} />  
+                    </div>
+                    <p>{style.text}</p>
+                </a>
+            ))}
         </div>
     )
 }
